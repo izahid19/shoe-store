5ac5eff5ac86945b251a394e4237651211a53db5
zahid mushtaq <mushtaqzahid888@gmail.com>
2023-08-19 21:55:19 +0530

    heared

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Header = () => {
-    return <div className="text">Header</div>;
+    const [mobileMenu, setMobileMenu] = useState(false);
+    const [showCatMenu, setShowCatMenu] = useState(false);
+    const [show, setShow] = useState("translate-y-0");
+    const [lastScrollY, setLastScrollY] = useState(0);
+    
+    return (<header className={` w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300`}>Header</header>);
 };
 
 export default Header;
